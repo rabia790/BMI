d1de7f1879674a332e536ec791d580192970967b
Rabia790 <rabiaangel9921@gmail.com>
2022-11-28 18:25:06 -0500

    Added result functionality

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mass, height;
     Button bmiButton;
     int bmi;
+    String cal;
+    TextView resulttxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         mass = findViewById(R.id.etMAss);
         height = findViewById(R.id.etHeight);
         bmiButton = findViewById(R.id.btnCalculate);
+        resulttxt = findViewById(R.id.result);
         bmiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,11 +33,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-private void calculateBmi(String mass, String height){
-        bmi = Integer.parseInt(mass)/Integer.parseInt(height);
-        Intent intent = new Intent(MainActivity.this, CalculatedBmiActivity.class);
-        intent.putExtra("key", String.valueOf(bmi));
-        startActivity(intent);
-}
+
+    private void calculateBmi(String mass, String height) {
+        bmi = Integer.parseInt(mass) / Integer.parseInt(height);
+        String s1 = String.valueOf(bmi);
+        resulttxt.setText(s1);
+
+        // Intent intent = new Intent(MainActivity.this, CalculatedBmiActivity.class);
+        //intent.putExtra("key", String.valueOf(bmi));
+        // startActivity(intent);
+    }
 
 }
